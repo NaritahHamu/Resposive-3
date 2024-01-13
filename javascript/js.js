bd087c0f1d5898c2ed1d2c14f32c9ce6d44e5805
NariNoodles <104844130+NaritahHamu@users.noreply.github.com>
2024-01-13 18:26:54 +0100

    Banner
    Added animated banner with a slight pause function when manually scrolled.
    
    Not perfect.

--- javascript/js.js
+++ javascript/js.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
+
+
+    forward();
 })
+let counter = 1;
 
 function menuToggle(){
     $("#menu-icon").toggleClass("menu-toggle-icon");
@@ -12,3 +16,42 @@ function navSelect(buttonID){
     console.log(id)
     setTimeout(menuToggle, 200)
 }
+
+//Banner
+
+  function forward(){
+    console.log("Counter "+ counter);
+    timeoutId = setTimeout(function(){ 
+    document.getElementById('radio' + counter).checked = true
+    if(counter >= 3){
+      counter = 2;
+      backward()
+    }else{
+      counter++;
+      forward();
+    }
+}, 7000);
+  }
+
+  function backward(){
+    console.log("Counter "+ counter);
+    timeoutId = setTimeout(function(){
+      document.getElementById('radio' + counter).checked = true
+
+      if(counter <= 1){
+        counter = 2;
+        forward()
+      }else{
+        counter--;
+        backward();
+      }
+    }, 7000);
+  }      
+  
+  function pause() {
+    clearTimeout(timeoutId);
+
+    setTimeout(function(){
+      forward()
+    }, 2000)
+  }
