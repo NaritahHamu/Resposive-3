0e08527d13cf17428831503608eb73307a3599c8
NariNoodles <104844130+NaritahHamu@users.noreply.github.com>
2024-01-20 12:14:48 +0100

    NAV/ Index / Services
    Mobile Nav now works properly.
    
    Switched to simple banner for the moment.
    
    Fixed home page image resizing, images are smaller and properly aligned to all resolutions.
    
    Services Page
    Added JS functionality for section selection. All data will be dynamic.

--- javascript/js.js
+++ javascript/js.js
@@ -1,57 +1,57 @@
-$(document).ready(function(){
+$(document).ready(function(){  
+  loadPage("/pages/services.html")
 
+  $("#main-wrapper").on('click', '[id*="btn-service"]', function () {
 
-    forward();
+    let condition =  $(this).attr("id");
+    let firstPart = condition.split(" ")[0];
+  
+    console.log(firstPart);
+
+    switch (firstPart) {
+        case 'BCS':
+        console.log('Building Control');
+        // Do something for Case 1
+        break;
+  
+        case 'FMS':
+        console.log('Facility Managment');
+        // Do something for Case 2
+        break;
+  
+        case 'FTTH':
+        console.log('FTTH / WAN / LAN');
+        // Do something for Case 3
+        break;
+  
+        case 'LPS':
+        console.log('Lighting Protection');
+        // Do something for Case 3
+        break;
+  
+        case 'TLC':
+        console.log('Tele communication');
+        // Do something for Case 3
+        break;
+  
+        default:
+        console.log('Default Case');
+        // Do something for the default case
+  };
+  });
+
 })
-let counter = 1;
 
 function menuToggle(){
     $("#menu-icon").toggleClass("menu-toggle-icon");
     $("#mobile-menu").toggleClass("menu-toggle-menu");
 }
 
-function navSelect(buttonID){
-    let id = buttonID
+function loadPage(page) {
+  $("#content").load(page);
 
-    console.log(id)
-    setTimeout(menuToggle, 200)
+  $("#menu-icon").removeClass("menu-toggle-icon");
+  $("#mobile-menu").removeClass("menu-toggle-menu");
 }
 
-//Banner
 
-  function forward(){
-    console.log("Counter "+ counter);
-    timeoutId = setTimeout(function(){ 
-    document.getElementById('radio' + counter).checked = true
-    if(counter >= 3){
-      counter = 2;
-      backward()
-    }else{
-      counter++;
-      forward();
-    }
-}, 7000);
-  }
-
-  function backward(){
-    console.log("Counter "+ counter);
-    timeoutId = setTimeout(function(){
-      document.getElementById('radio' + counter).checked = true
-
-      if(counter <= 1){
-        counter = 2;
-        forward()
-      }else{
-        counter--;
-        backward();
-      }
-    }, 7000);
-  }      
-  
-  function pause() {
-    clearTimeout(timeoutId);
-
-    setTimeout(function(){
-      forward()
-    }, 2000)
-  }
