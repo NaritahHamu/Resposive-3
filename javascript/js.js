8cd34041e197d7e4d44b6dd70537974c805ce6a6
NariNoodles <104844130+NaritahHamu@users.noreply.github.com>
2024-01-26 13:31:42 +0100

    Banner
    Added Banner with animation, it's not perfect but it works. It's mostly JS. Will switch to keyframes in the future.

--- javascript/js.js
+++ javascript/js.js
@@ -1,15 +1,13 @@
 $(document).ready(function(){  
-  loadPage("/pages/services.html")
+  loadPage("/pages/home.html");
 
   $("#main-wrapper").on('click', '[id*="btn-service"]', function () {
 
     let condition =  $(this).attr("id");
     let firstPart = condition.split(" ")[0];
 
-    
-
     servicesSelect(firstPart);
-    active(this);
+    serviceTabActive(this);
       
   });
 
@@ -17,13 +15,12 @@ $(document).ready(function(){
     let x = $(this).attr("id");
     let y = x.split(" ")[1];
 
-    function toggleActive2(x){
+    function toggleServiceActive(x){
       console.log(x);
       $(".active").removeClass("active");
       $("." + x).addClass("active");
     }
 
-    // Wrap the asynchronous operation in a promise
     let contentLoadPromise = new Promise(function(resolve, reject) {
         $("#content").load("/pages/services.html", function(response, status, xhr) {
             if (status == "success") {
@@ -34,10 +31,9 @@ $(document).ready(function(){
         });
     });
 
-    // After content is loaded, call servicesSelect
     contentLoadPromise.then(function() {
         servicesSelect(y);
-        toggleActive2(y);
+        toggleServiceActive(y);
     }).catch(function(error) {
         console.error(error);
     });
@@ -49,7 +45,6 @@ function servicesSelect(data){
 
   let servicesTitle = $("<h3></h3>").addClass("services-title")
 
-  let class2 = data;
 
   if ($(".services-title").length > 0) {
   } else {
@@ -66,7 +61,7 @@ function servicesSelect(data){
         $(".services-img:nth-child(3)").css("background-image","url('/images/gallery/home-gallery/go-cabling.png')")
         $(".services-img:nth-child(4)").css("background-image","url('/images/gallery/home-gallery/go-cabling.png')")
         
-        active(data);
+        serviceTabActive(data);
         break;
 
       case 'FMS':
@@ -78,7 +73,7 @@ function servicesSelect(data){
         $(".services-img:nth-child(3)").css("background-image","url('/images/gallery/home-gallery/mobile-antenna.png')")
         $(".services-img:nth-child(4)").css("background-image","url('/images/gallery/home-gallery/mobile-antenna.png')")
         
-        active(data);
+        serviceTabActive(data);
         break;
 
       case 'FTTH':
@@ -90,7 +85,7 @@ function servicesSelect(data){
         $(".services-img:nth-child(3)").css("background-image","url('/images/gallery/home-gallery/solar-panels.png')")
         $(".services-img:nth-child(4)").css("background-image","url('/images/gallery/home-gallery/solar-panels.png')")
         
-        active(data);
+        serviceTabActive(data);
         break;
 
       case 'LPS':
@@ -102,7 +97,7 @@ function servicesSelect(data){
         $(".services-img:nth-child(3)").css("background-image","url('/images/gallery/home-gallery/IMG_1708.jpg')")
         $(".services-img:nth-child(4)").css("background-image","url('/images/gallery/home-gallery/IMG_1708.jpg')")
         
-        active(data);
+        serviceTabActive(data);
         break;
 
       case 'TLC':
@@ -114,17 +109,13 @@ function servicesSelect(data){
         $(".services-img:nth-child(3)").css("background-image","url('/images/gallery/home-gallery/go-cabling.png')")
         $(".services-img:nth-child(4)").css("background-image","url('/images/gallery/home-gallery/IMG_1708.jpg')")
 
-        active(data);
+        serviceTabActive(data);
         break;
 
       default:
       console.log('Default Case');
   };
 }
-
-
-
-
 function menuToggle(){
     $("#menu-icon").toggleClass("menu-toggle-icon");
     $("#mobile-menu").toggleClass("menu-toggle-menu");
@@ -134,10 +125,60 @@ function loadPage(page) {
 
   $("#menu-icon").removeClass("menu-toggle-icon");
   $("#mobile-menu").removeClass("menu-toggle-menu");
-}
 
-function active(x){
+  loadBanner(page);
+}
+function serviceTabActive(x){
   console.log(x);
   $(".active").removeClass("active");
   $(x).addClass("active");
+}
+function loadBanner(page) {
+  return new Promise(function(resolve, reject) {
+    if (page == "/pages/home.html") {
+      // Simulate asynchronous loading of the dynamic content
+      setTimeout(function() {
+        resolve($("Resolved"));
+        animateBanner();
+        
+      }, 0); // Adjust the delay based on your actual loading time
+    } else {
+      reject("Page is not home.html");
+    }
+  });
+}
+
+function animateBanner(){
+  setTimeout(function(){
+    $("#bc_two").css("z-index", "1");
+    $("#bc_one").css("transform", "translateX(-50%)");
+    $("#bc_two").css("transform", "translateX(50%)");
+  }, 5000);
+  setTimeout(function(){
+    $("#bc_one").css("transform", "translateX(-100%)");
+    $("#bc_two").css("transform", "translateX(0%)");
+  }, 10000);
+  setTimeout(function(){
+    $("#bc_one").css("transform", "translateX(50%)");
+    $("#bc_two").css("transform", "translateX(-50%)");
+    reset("one");
+  }, 15000);
+  setTimeout(function(){
+    $("#bc_one").css("z-index", "1");
+    $("#bc_one").css("transform", "translateX(50%)");
+    $("#bc_two").css("transform", "translateX(-50%)");
+  }, 20000);
+  setTimeout(function(){
+    $("#bc_one").css("transform", "translateX(0%)");
+    $("#bc_two").css("transform", "translateX(-100%)");
+  }, 25000);
+  setTimeout(function(){
+    animateBanner();
+    reset("two");
+  }, 26000);
+}
+
+function reset(banner){
+  $("#bc_"+banner).css("transform", "translateX(50%)");
+  $("#bc_"+banner).css("z-index", "-1");
 }
